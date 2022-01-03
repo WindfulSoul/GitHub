b8f8c15d3087417481b15f6e00a857c5d15530e2
Song Aiden Ko <81278660+WindfulSoul@users.noreply.github.com>
2022-01-02 20:14:50 -0800

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>Github Finders</p>
+      <p>Version: 3.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p className='lead'>This page does not exist, please refer back to the homepage</p>
+    </div>
+  );
+};
+
+export default NotFound;
